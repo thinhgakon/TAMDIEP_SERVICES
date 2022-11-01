5824b2abead23f61cd6f85bd74f000ed6141be5b
DESKTOP-GGQMM0O\Mango <toi.la.dan.xu.nghe@gmail.com>
2022-11-01 10:45:07 +0700

    Gateway: add khung service

---
+++ XHTD_SERVICES_GATEWAY/DIBootstrapper.cs
@@ -0,0 +1,47 @@
+﻿using Autofac.Extras.Quartz;
+using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XHTD_SERVICES_GATEWAY.Schedules;
+using XHTD_SERVICES.Data.Repositories;
+using XHTD_SERVICES_GATEWAY.Jobs;
+using XHTD_SERVICES.Data.Entities;
+
+namespace XHTD_SERVICES_GATEWAY
+{
+    public static class DIBootstrapper
+    {
+        public static IContainer Init()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<XHTD_Entities>().AsSelf();
+            builder.RegisterType<StoreOrderOperatingRepository>().AsSelf();
+
+            RegisterScheduler(builder);
+
+            return builder.Build();
+        }
+
+        private static void RegisterScheduler(ContainerBuilder builder)
+        {
+            var schedulerConfig = new NameValueCollection {
+              {"quartz.threadPool.threadCount", "20"},
+              {"quartz.scheduler.threadName", "MyScheduler"}
+            };
+
+            builder.RegisterModule(new QuartzAutofacFactoryModule
+            {
+                ConfigurationProvider = c => schedulerConfig
+            });
+
+            builder.RegisterModule(new QuartzAutofacJobsModule(typeof(GatewayModuleJob).Assembly));
+            builder.RegisterType<JobScheduler>().AsSelf();
+        }
+    }
+}
---
+++ XHTD_SERVICES_GATEWAY/Jobs/GatewayModuleJob.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using log4net;
+using XHTD_SERVICES.Data.Repositories;
+using RestSharp;
+using XHTD_SERVICES_GATEWAY.Models.Response;
+using XHTD_SERVICES.Data.Models.Response;
+using XHTD_SERVICES_GATEWAY.Models.Request;
+using Newtonsoft.Json;
+using System.Configuration;
+using System.Collections.Specialized;
+using XHTD_SERVICES_GATEWAY.Models.Values;
+
+namespace XHTD_SERVICES_GATEWAY.Jobs
+{
+    public class GatewayModuleJob : IJob
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected readonly StoreOrderOperatingRepository _storeOrderOperatingRepository;
+
+        private static string strToken;
+
+        public GatewayModuleJob(StoreOrderOperatingRepository storeOrderOperatingRepository)
+        {
+            _storeOrderOperatingRepository = storeOrderOperatingRepository;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            await Task.Run(() =>
+            {
+                log.Info("start GatewayModule Job");
+                Console.WriteLine("start GatewayModule Job");
+            });
+        }
+    }
+}
