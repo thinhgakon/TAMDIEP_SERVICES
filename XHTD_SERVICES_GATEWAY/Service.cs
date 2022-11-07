93134b7b85ac3eb4adc485ad47f10d1f99ac2fa1
DESKTOP-GGQMM0O\Mango <toi.la.dan.xu.nghe@gmail.com>
2022-11-07 14:13:16 +0700

    Gateway: add installer service

--- XHTD_SERVICES_GATEWAY/Service.cs
+++ XHTD_SERVICES_GATEWAY/Service.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
+using log4net;
+using XHTD_SERVICES_GATEWAY;
+using XHTD_SERVICES_GATEWAY.Schedules;
 
 namespace XHTD_SERVICES_GATEWAY
 {
     public partial class Service : ServiceBase
     {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public Service()
         {
             InitializeComponent();
@@ -19,10 +25,14 @@ namespace XHTD_SERVICES_GATEWAY
 
         protected override void OnStart(string[] args)
         {
+            log.Info("OnStart service GATEWAY");
+            Autofac.IContainer container = DIBootstrapper.Init();
+            container.Resolve<JobScheduler>().Start();
         }
 
         protected override void OnStop()
         {
+            log.Info("OnStop service GATEWAY");
         }
     }
 }
