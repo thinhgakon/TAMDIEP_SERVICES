4168d5fbcb606a3ff6ef36eb09981bbae571b05e
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-11-10 09:01:31 +0700

    Add project Helper

---
+++ XHTD_SERVICES.Helper/HttpRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using XHTD_SERVICES.Helper.Models.Request;
+using XHTD_SERVICES.Helper.Models.Response;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace XHTD_SERVICES.Helper
+{
+    public static class HttpRequest
+    {
+        public static IRestResponse GetWebsaleToken()
+        {
+            var apiUrl = ConfigurationManager.GetSection("API_WebSale/Url") as NameValueCollection;
+            var account = ConfigurationManager.GetSection("API_WebSale/Account") as NameValueCollection;
+
+            var requestData = new GetTokenRequest
+            {
+                grant_type = account["grant_type"].ToString(),
+                client_secret = account["client_secret"].ToString(),
+                username = account["username"].ToString(),
+                password = account["password"].ToString(),
+                client_id = account["client_id"].ToString(),
+            };
+
+            var client = new RestClient(apiUrl["GetToken"]);
+            var request = new RestRequest();
+
+            request.Method = Method.POST;
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "multipart/form-data");
+            request.Parameters.Clear();
+            request.AddParameter("grant_type", requestData.grant_type);
+            request.AddParameter("client_secret", requestData.client_secret);
+            request.AddParameter("username", requestData.username);
+            request.AddParameter("password", requestData.password);
+            request.AddParameter("client_id", requestData.client_id);
+
+            IRestResponse response = client.Execute(request);
+
+            return response;
+        }
+    }
+}
