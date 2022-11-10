bbb7082aed47d2311a0650020fe753a4904a8452
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-11-10 22:29:13 +0700

    Tram951: add Calculator helper

---
+++ XHTD_SERVICES.Helper/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHTD_SERVICES.Helper
+{
+    public static class Calculator
+    {
+        public static bool CheckBalanceValues(List<int> numbers, int saiso)
+        {
+            if (numbers.Count < 5)
+            {
+                return false;
+            }
+
+            var tbc = TrungBinhCong(numbers);
+
+            foreach(int number in numbers)
+            {
+                if(Math.Abs(number - tbc) > saiso)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static double TrungBinhCong(List<int> numbers)
+        {
+            int size = numbers.Count;
+            if (size == 0)
+            {
+                return 0;
+            }
+
+            int total = 0;
+            foreach(int number in numbers)
+            {
+                total = total + number;
+            }
+
+            return (float)total / size;
+        }
+    }
+}
