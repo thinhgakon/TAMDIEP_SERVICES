2dda3a3ead4610c58d225335601af681dfe78b5b
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-11-16 07:52:08 +0700

    SyncOrder: update config

--- XHTD_SERVICES_SYNC_ORDER/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_SYNC_ORDER/ProjectInstaller.Designer.cs
@@ -40,6 +40,7 @@
             // serviceInstaller1
             // 
             this.serviceInstaller1.ServiceName = "HPSyncOrderService";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
             // 
