7d5d6bee0c221b46fd4148f754b2070787d82102
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-11-17 11:40:56 +0700

    QueueToCall: init project

---
+++ XHTD_SERVICES_QUEUE_TO_CALL/Jobs/QueueToCallJob.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz;
+using log4net;
+using XHTD_SERVICES.Data.Repositories;
+using RestSharp;
+using XHTD_SERVICES.Data.Models.Response;
+using Newtonsoft.Json;
+using XHTD_SERVICES.Helper;
+using XHTD_SERVICES.Helper.Models.Request;
+
+namespace XHTD_SERVICES_QUEUE_TO_CALL.Jobs
+{
+    public class QueueToCallJob : IJob
+    {
+        protected readonly StoreOrderOperatingRepository _storeOrderOperatingRepository;
+
+        protected readonly VehicleRepository _vehicleRepository;
+
+        protected readonly Notification _notification;
+
+        protected readonly QueueToCallLogger _syncOrderLogger;
+
+        private static string strToken;
+
+        public QueueToCallJob(
+            StoreOrderOperatingRepository storeOrderOperatingRepository,
+            VehicleRepository vehicleRepository,
+            Notification notification,
+            QueueToCallLogger syncOrderLogger
+            )
+        {
+            _storeOrderOperatingRepository = storeOrderOperatingRepository;
+            _vehicleRepository = vehicleRepository;
+            _notification = notification;
+            _syncOrderLogger = syncOrderLogger;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            await Task.Run(async () =>
+            {
+                await SyncOrderProcess();
+            });
+        }
+
+        public async Task SyncOrderProcess()
+        {
+            _syncOrderLogger.LogInfo("start process QueueToCallJob");
+        }
+    }
+}
