256d177ee19f271907e22b8358495b5f39421589
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-11-17 21:38:48 +0700

    CallInTrough: config service

--- XHTD_DEBUG/Program.cs
+++ XHTD_DEBUG/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Autofac;
 using log4net;
-using XHTD_SERVICES_QUEUE_TO_CALL;
-using XHTD_SERVICES_QUEUE_TO_CALL.Schedules;
+using XHTD_SERVICES_CALL_IN_TROUGH;
+using XHTD_SERVICES_CALL_IN_TROUGH.Schedules;
 
 namespace XHTD_DEBUG
 {
--- XHTD_SERVICES_CALL_IN_TROUGH/Jobs/CallInTroughJob.cs
+++ XHTD_SERVICES_CALL_IN_TROUGH/Jobs/CallInTroughJob.cs
@@ -55,74 +55,13 @@ namespace XHTD_SERVICES_CALL_IN_TROUGH.Jobs
 
             await Task.Run(() =>
             {
-                QueueToCallProcess();
+                CallInTroughProcess();
             });
         }
 
-        public void QueueToCallProcess()
+        public void CallInTroughProcess()
         {
-            _callInTroughLogger.LogInfo("start process QueueToCallJob");
-
-            SyncTrough("M1");
-        }
-
-        public async void SyncTrough(string troughCode)
-        {
-            _callInTroughLogger.LogInfo($"SyncTrough {troughCode}");
-
-            // lay thong tin máng
-            var troughInfo = _troughRepository.GetDetail(troughCode);
-            if (troughInfo == null)
-            { 
-                return; 
-            }
-
-            var currentDeliveryCodeInTrough = troughInfo.DeliveryCodeCurrent;
-
-            // Nếu hiện tại đang có đơn trong máng
-            if (!String.IsNullOrEmpty(currentDeliveryCodeInTrough)) 
-            {
-                // update through line cho don hang
-                await _storeOrderOperatingRepository.UpdateTroughLine(currentDeliveryCodeInTrough, troughCode);
-
-                // update step theo % xuat hang cua don hang tai mang
-                var isAlmostDone = (troughInfo.CountQuantityCurrent / troughInfo.PlanQuantityCurrent) > 0.8;
-                if (isAlmostDone)
-                {
-                    await _storeOrderOperatingRepository.UpdateStepInTrough(currentDeliveryCodeInTrough, (int)OrderStep.DA_LAY_HANG);
-                }
-                else
-                {
-                    await _storeOrderOperatingRepository.UpdateStepInTrough(currentDeliveryCodeInTrough, (int)OrderStep.DANG_LAY_HANG);
-                }
-            }
-
-            // Đếm số lượng đơn hàng đang chờ gọi của máng
-            var numberOrderFrontTrough = _callToTroughRepository.GetNumberOrderInQueue(troughCode);
-
-            if(numberOrderFrontTrough < MAX_ORDER_IN_QUEUE_TO_CALL)
-            {
-                // goi them xe vao hang doi
-                PushOrderToQueue(troughCode, MAX_ORDER_IN_QUEUE_TO_CALL - numberOrderFrontTrough);
-            }
-        }
-
-        public async void PushOrderToQueue(string troughcode, int quantity)
-        {
-            var orders = await _storeOrderOperatingRepository.GetOrdersSortByIndex(quantity);
-            if (orders == null || orders.Count == 0)
-            {
-                return;
-            }
-
-            foreach (var order in orders)
-            {
-                // Cap nhat trang thai don hang DANG_GOI_XE
-                await _storeOrderOperatingRepository.UpdateStepDangGoiXe(order.DeliveryCode);
-
-                // Them ban ghi vao tblCallToTrough: danh sach cho goi xe
-                await _callToTroughRepository.CreateAsync(order.Id, troughcode);
-            }
+            _callInTroughLogger.LogInfo("start process CallInTroughJob");
         }
     }
 }
