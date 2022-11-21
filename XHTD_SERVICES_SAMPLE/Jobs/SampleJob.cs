262080b0d655e03c9406abdde75a2c95b3d9bdc5
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-11-21 11:57:44 +0700

    SampleService: init project

---
+++ XHTD_SERVICES_SAMPLE/Jobs/SampleJob.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz;
+using log4net;
+using XHTD_SERVICES.Data.Repositories;
+using XHTD_SERVICES.Data.Models.Response;
+using Newtonsoft.Json;
+using XHTD_SERVICES.Helper;
+using XHTD_SERVICES.Helper.Models.Request;
+using XHTD_SERVICES.Data.Models.Values;
+using System.Threading;
+
+namespace XHTD_SERVICES_SAMPLE.Jobs
+{
+    public class SampleJob : IJob
+    {
+        protected readonly StoreOrderOperatingRepository _storeOrderOperatingRepository;
+
+        protected readonly VehicleRepository _vehicleRepository;
+
+        protected readonly TroughRepository _troughRepository;
+
+        protected readonly CallToTroughRepository _callToTroughRepository;
+
+        protected readonly Notification _notification;
+
+        protected readonly SampleLogger _callInTroughLogger;
+
+        const int MAX_ORDER_IN_QUEUE_TO_CALL = 2;
+
+        public SampleJob(
+            StoreOrderOperatingRepository storeOrderOperatingRepository,
+            VehicleRepository vehicleRepository,
+            TroughRepository troughRepository,
+            CallToTroughRepository callToTroughRepository,
+            Notification notification,
+            SampleLogger callInTroughLogger
+            )
+        {
+            _storeOrderOperatingRepository = storeOrderOperatingRepository;
+            _vehicleRepository = vehicleRepository;
+            _troughRepository = troughRepository;
+            _callToTroughRepository = callToTroughRepository;
+            _notification = notification;
+            _callInTroughLogger = callInTroughLogger;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            await Task.Run(() =>
+            {
+                SampleProcess();
+            });
+        }
+
+        public void SampleProcess()
+        {
+            _callInTroughLogger.LogInfo("start process SampleJob");
+        }
+    }
+}
