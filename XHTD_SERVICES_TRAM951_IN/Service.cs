a4c70256413efa62e8ee9b0c044e15ccfb037528
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-12-27 10:39:50 +0700

    Tram951_IN

---
+++ XHTD_SERVICES_TRAM951_IN/Service.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using log4net;
+using System.ServiceProcess;
+using XHTD_SERVICES_TRAM951_IN.Schedules;
+
+namespace XHTD_SERVICES_TRAM951_IN
+{
+    public partial class Service : ServiceBase
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log.Info("OnStart service TRAM951");
+            Autofac.IContainer container = DIBootstrapper.Init();
+            container.Resolve<JobScheduler>().Start();
+        }
+
+        protected override void OnStop()
+        {
+            log.Info("OnStop service TRAM951");
+        }
+    }
+}
