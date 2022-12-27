08d59d7725a584feb4c48ec5ac69b7845e6a040f
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2022-12-27 14:56:40 +0700

    Tram951: update scale hub

--- XHTD_SERVICES_TRAM951_IN/Hubs/ScaleHub.cs
+++ XHTD_SERVICES_TRAM951_IN/Hubs/ScaleHub.cs
@@ -3,15 +3,48 @@ using Microsoft.AspNet.SignalR;
 using Microsoft.Owin.Hosting;
 using Owin;
 using Microsoft.Owin.Cors;
+using log4net;
 
 namespace XHTD_SERVICES_TRAM951_IN.Hubs
 {
     public class ScaleHub : Hub
     {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(ScaleHub));
+
         public void Send(string name, string message)
         {
             Clients.All.addMessage(name, message);
             Console.WriteLine("send send");
+        }
+
+        public void Send9511ScaleInfo(DateTime time, string value)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.Send9511ScaleInfo(time, value);
+            ReadDataScale9511(time, value);
+        }
+
+        public void Send9512ScaleInfo(DateTime time, string value)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.Send9512ScaleInfo(time, value);
+            ReadDataScale9511(time, value);
+        }
+
+        public void SendClinkerScaleInfo(DateTime time, string value)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.SendClinkerScaleInfo(time, value);
+        }
+
+        public void ReadDataScale9511(DateTime time, string value)
+        {
+            logger.Info($"Received 951-1 data: time={time}, value={value}");
+        }
+
+        public void ReadDataScale9512(DateTime time, string value)
+        {
+            logger.Info($"Received 951-2 data: time={time}, value={value}");
         }
     }
 }
