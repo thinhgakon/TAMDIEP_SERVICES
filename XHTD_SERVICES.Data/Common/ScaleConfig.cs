24b63263a5f72b27c58cbeb327d9b61a132c7faa
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2023-01-10 18:23:49 +0700

    Cân tối thiểu 20 lần

--- XHTD_SERVICES.Data/Common/ScaleConfig.cs
+++ XHTD_SERVICES.Data/Common/ScaleConfig.cs
@@ -8,7 +8,7 @@ namespace XHTD_SERVICES.Data.Common
 {
     public static class ScaleConfig
     {
-        public static readonly int MAX_LENGTH_SCALE_VALUE = 10;
+        public static readonly int MAX_LENGTH_SCALE_VALUE = 20;
 
         public static readonly int MIN_WEIGHT_VEHICLE = 30;
 
