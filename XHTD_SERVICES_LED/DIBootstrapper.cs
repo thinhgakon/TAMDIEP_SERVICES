7d779ec95516cea0dbf3d14e8f19871ab543969e
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2023-01-14 20:55:01 +0700

    Init project LED

---
+++ XHTD_SERVICES_LED/DIBootstrapper.cs
@@ -0,0 +1,46 @@
+﻿using Autofac.Extras.Quartz;
+using Autofac;
+using System.Collections.Specialized;
+using XHTD_SERVICES_AUTO_REINDEX.Schedules;
+using XHTD_SERVICES.Data.Repositories;
+using XHTD_SERVICES_AUTO_REINDEX.Jobs;
+using XHTD_SERVICES.Data.Entities;
+using XHTD_SERVICES.Helper;
+
+namespace XHTD_SERVICES_AUTO_REINDEX
+{
+    public static class DIBootstrapper
+    {
+        public static IContainer Init()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<XHTD_Entities>().AsSelf();
+            builder.RegisterType<StoreOrderOperatingRepository>().AsSelf();
+            builder.RegisterType<VehicleRepository>().AsSelf();
+            builder.RegisterType<SystemParameterRepository>().AsSelf();
+            builder.RegisterType<Notification>().AsSelf();
+            builder.RegisterType<AutoReindexLogger>().AsSelf();
+
+            RegisterScheduler(builder);
+
+            return builder.Build();
+        }
+
+        private static void RegisterScheduler(ContainerBuilder builder)
+        {
+            var schedulerConfig = new NameValueCollection {
+              {"quartz.threadPool.threadCount", "20"},
+              {"quartz.scheduler.threadName", "MyScheduler"}
+            };
+
+            builder.RegisterModule(new QuartzAutofacFactoryModule
+            {
+                ConfigurationProvider = c => schedulerConfig
+            });
+
+            builder.RegisterModule(new QuartzAutofacJobsModule(typeof(AutoReindexJob).Assembly));
+            builder.RegisterType<JobScheduler>().AsSelf();
+        }
+    }
+}
---
+++ XHTD_SERVICES_LED/Schedules/JobScheduler.cs
@@ -0,0 +1,61 @@
+﻿using Quartz.Impl;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using XHTD_SERVICES_AUTO_REINDEX.Jobs;
+using System.Configuration;
+
+namespace XHTD_SERVICES_AUTO_REINDEX.Schedules
+{
+    public class JobScheduler
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly IScheduler _scheduler;
+
+        public JobScheduler(IScheduler scheduler)
+        {
+            _scheduler = scheduler;
+        }
+
+        public async void Start()
+        {
+            await _scheduler.Start();
+
+            // Đồng bộ đơn hàng
+            IJobDetail syncOrderJob = JobBuilder.Create<AutoReindexJob>().Build();
+            ITrigger syncOrderTrigger = TriggerBuilder.Create()
+                .WithPriority(1)
+                 .StartNow()
+                 .WithSimpleSchedule(x => x
+                     .WithIntervalInSeconds(Convert.ToInt32(ConfigurationManager.AppSettings.Get("Sync_Order_Interval_In_Seconds")))
+                    .RepeatForever())
+                .Build();
+            await _scheduler.ScheduleJob(syncOrderJob, syncOrderTrigger);
+
+            IJobDetail showLedBaoJob = JobBuilder.Create<LedHPTestXibao>().Build();
+            ITrigger showLedBaoTrigger = TriggerBuilder.Create()
+                .WithPriority(1)
+                 .StartNow()
+                 .WithSimpleSchedule(x => x
+                     .WithIntervalInSeconds(Convert.ToInt32(ConfigurationManager.AppSettings.Get("Show_Led_Bao_In_Seconds")))
+                    .RepeatForever())
+                .Build();
+            await _scheduler.ScheduleJob(showLedBaoJob, showLedBaoTrigger);
+
+            IJobDetail showLedRoiJob = JobBuilder.Create<LedHPTestXiroi>().Build();
+            ITrigger showLedRoiTrigger = TriggerBuilder.Create()
+                .WithPriority(1)
+                 .StartNow()
+                 .WithSimpleSchedule(x => x
+                     .WithIntervalInSeconds(Convert.ToInt32(ConfigurationManager.AppSettings.Get("Show_Led_Roi_In_Seconds")))
+                    .RepeatForever())
+                .Build();
+            await _scheduler.ScheduleJob(showLedRoiJob, showLedRoiTrigger);
+        }
+    }
+}
