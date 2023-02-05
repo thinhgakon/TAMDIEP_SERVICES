2ed475aa3ef7e901da17d09f908b5a43d4dddf9d
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2023-02-06 00:01:34 +0700

    update install service type

--- XHTD_SERVICES_CALL_IN_TROUGH/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_CALL_IN_TROUGH/ProjectInstaller.Designer.cs
@@ -40,6 +40,7 @@
             // serviceInstaller1
             // 
             this.serviceInstaller1.ServiceName = "HPCallInTroughService";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
             // 
