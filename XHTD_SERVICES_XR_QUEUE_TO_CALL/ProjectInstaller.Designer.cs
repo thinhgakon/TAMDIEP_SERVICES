1bfb9a88dc80be0a6753854eaae55e166af27bcc
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2023-02-06 17:02:14 +0700

    update service name

--- XHTD_SERVICES_XR_QUEUE_TO_CALL/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_XR_QUEUE_TO_CALL/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "HPQueueToCallService";
+            this.serviceInstaller1.ServiceName = "HPXRQueueToCallService";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
