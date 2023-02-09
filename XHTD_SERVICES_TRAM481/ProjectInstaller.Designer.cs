1c1b070741ce768c5a8ec8eb09d57a77d7ea7b59
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2023-02-09 23:32:29 +0700

    update start type

--- XHTD_SERVICES_TRAM481/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_TRAM481/ProjectInstaller.Designer.cs
@@ -40,6 +40,7 @@
             // serviceInstaller1
             // 
             this.serviceInstaller1.ServiceName = "HPTram481Service";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
             // 
