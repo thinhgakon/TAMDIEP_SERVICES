6224b1892e22890289653e36f42f64ccd513c1e3
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2023-02-18 21:39:50 +0700

    Tram951_1: kiểm tra độ lệch khối lượng cân

--- XHTD_SERVICES_TRAM951_1/Business/DesicionScaleBusiness.cs
+++ XHTD_SERVICES_TRAM951_1/Business/DesicionScaleBusiness.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XHTD_SERVICES.Data.Entities;
 using XHTD_SERVICES.Data.Repositories;
 using XHTD_SERVICES.Device;
 using XHTD_SERVICES_TRAM951_1.Models.Response;
@@ -14,11 +15,15 @@ namespace XHTD_SERVICES_TRAM951_1.Business
     {
         protected readonly ScaleOperatingRepository _scaleOperatingRepository;
 
+        protected readonly StoreOrderOperatingRepository _storeOrderOperatingRepository;
+
         public DesicionScaleBusiness(
-            ScaleOperatingRepository scaleOperatingRepository
+            ScaleOperatingRepository scaleOperatingRepository,
+            StoreOrderOperatingRepository storeOrderOperatingRepository
             )
         {
             _scaleOperatingRepository = scaleOperatingRepository;
+            _storeOrderOperatingRepository = storeOrderOperatingRepository;
         }
 
         public DesicionScaleResponse MakeDecisionScaleIn(string deliveryCode, int weight)
@@ -28,11 +33,57 @@ namespace XHTD_SERVICES_TRAM951_1.Business
             return response;
         }
 
-        public DesicionScaleResponse MakeDecisionScaleOut(string deliveryCode, int weight)
+        public async Task<DesicionScaleResponse> MakeDecisionScaleOut(string deliveryCode, int weight)
         {
+            var resultResponse = new DesicionScaleResponse
+            {
+                Code = "02",
+                Message = "Cân thất bại"
+            };
+
+            var order = await _storeOrderOperatingRepository.GetDetail(deliveryCode);
+
+            if (CheckToleranceLimit(order, weight))
+            {
+                // vi phạm độ lệch khối lượng
+                return new DesicionScaleResponse
+                {
+                    Code = "02",
+                    Message = "Vượt quá 1% dung sai cho phép"
+                };
+            }
+
             var response = DIBootstrapper.Init().Resolve<ScaleApiLib>().ScaleOut(deliveryCode, weight);
 
-            return response;
+            if (response.Code == "01")
+            {
+                // Gọi API lưu thành công
+            }
+            else
+            {
+                // Gọi API lưu thất bại
+            }
+
+            resultResponse.Code = response.Code;
+            resultResponse.Message = response.Message;
+
+            return resultResponse;
+        }
+
+        public bool CheckToleranceLimit(tblStoreOrderOperating order, int weight)
+        {
+            bool isCheck = false;
+            try
+            {
+                var tolerance = (weight - order.WeightIn - order.SumNumber * 1000) / (order.SumNumber * 1000);
+                tolerance = tolerance < 0 ? (-1) * tolerance : tolerance;
+                isCheck = (double)tolerance > 0.01 ? true : false;
+            }
+            catch (Exception ex)
+            {
+                // TODO: log here
+            }
+            return isCheck;
         }
     }
 }
