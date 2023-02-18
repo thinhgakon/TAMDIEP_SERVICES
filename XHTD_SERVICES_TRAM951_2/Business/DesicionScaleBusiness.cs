564f5bf1147147b6c926071593fd45b04ba5c811
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2023-02-18 18:42:34 +0700

    update code

---
+++ XHTD_SERVICES_TRAM951_2/Business/DesicionScaleBusiness.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XHTD_SERVICES.Data.Repositories;
+using XHTD_SERVICES.Device;
+using XHTD_SERVICES_TRAM951_2.Models.Response;
+
+namespace XHTD_SERVICES_TRAM951_2.Business
+{
+    public class DesicionScaleBusiness
+    {
+        protected readonly ScaleOperatingRepository _scaleOperatingRepository;
+
+        public DesicionScaleBusiness(
+            ScaleOperatingRepository scaleOperatingRepository
+            )
+        {
+            _scaleOperatingRepository = scaleOperatingRepository;
+        }
+
+        public DesicionScaleResponse MakeDecisionScaleIn(string deliveryCode, int weight)
+        {
+            var response = DIBootstrapper.Init().Resolve<ScaleApiLib>().ScaleIn(deliveryCode, weight);
+
+            return response;
+        }
+
+        public DesicionScaleResponse MakeDecisionScaleOut(string deliveryCode, int weight)
+        {
+            var response = DIBootstrapper.Init().Resolve<ScaleApiLib>().ScaleOut(deliveryCode, weight);
+
+            return response;
+        }
+    }
+}
---
+++ XHTD_SERVICES_TRAM951_2/Business/ScaleApiLib.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XHTD_SERVICES_TRAM951_2.Models.Response;
+
+namespace XHTD_SERVICES_TRAM951_2.Business
+{
+    public class ScaleApiLib
+    {
+        public DesicionScaleResponse ScaleIn(string deliveryCode, int weight)
+        {
+            var response = new DesicionScaleResponse();
+            response.Code = "01";
+            response.Message = "Cân vào thành công";
+
+            return response;
+        }
+
+        public DesicionScaleResponse ScaleOut(string deliveryCode, int weight)
+        {
+            var response = new DesicionScaleResponse();
+            response.Code = "01";
+            response.Message = "Cân ra thành công";
+
+            return response;
+        }
+    }
+}
