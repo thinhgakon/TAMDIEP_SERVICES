a070b7d1be204155f680552ea5b7a6ce8e1fbdd3
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2023-02-19 14:10:37 +0700

    Tram951: xác định đơn hàng trên cân

--- XHTD_SERVICES.Data/Repositories/StoreOrderOperatingRepository.cs
+++ XHTD_SERVICES.Data/Repositories/StoreOrderOperatingRepository.cs
@@ -340,9 +340,21 @@ namespace XHTD_SERVICES.Data.Repositories
             using (var dbContext = new XHTD_Entities())
             {
                 var orders = await dbContext.tblStoreOrderOperatings
-                                            .Where(x => x.CardNo == cardNo 
-                                                        && x.Step <= (int)OrderStep.DA_CAN_RA
+                                            .Where(x => x.CardNo == cardNo
+                                                        && (
+                                                            (
+                                                                (x.CatId == "CLINKER" || x.TypeXK == "JUMBO" || x.TypeXK == "SLING")
+                                                                &&
+                                                                x.Step < (int)OrderStep.DA_CAN_RA
+                                                            )
+                                                        ||
+                                                            (
+                                                                (x.DriverUserName ?? "") != ""
+                                                                &&
+                                                                x.Step < (int)OrderStep.DA_CAN_RA
+                                                            )
                                                         )
+                                                   )
                                             .OrderByDescending(x => x.Step)
                                             .FirstOrDefaultAsync();
 
