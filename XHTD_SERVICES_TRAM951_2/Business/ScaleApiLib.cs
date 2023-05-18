f49055cdb189609142079e8181bee635c285b9fa
ThinhNX <toi.la.dan.xu.nghe@gmail.com>
2023-05-18 09:43:20 +0700

    Update code

--- XHTD_SERVICES_TRAM951_2/Business/ScaleApiLib.cs
+++ XHTD_SERVICES_TRAM951_2/Business/ScaleApiLib.cs
@@ -15,7 +15,7 @@ namespace XHTD_SERVICES_TRAM951_2.Business
 
             var strToken = HttpRequest.GetScaleToken();
 
-            var updateResponse = HttpRequest.UpdateWeightInWebSale(strToken, "045210-23", 100);
+            var updateResponse = HttpRequest.UpdateWeightInWebSale(strToken, deliveryCode, weight);
 
             if (updateResponse.StatusDescription.Equals("Unauthorized"))
             {
