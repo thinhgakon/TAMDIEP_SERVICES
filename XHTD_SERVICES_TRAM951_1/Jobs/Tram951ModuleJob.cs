ddcaddc3f1ee60d2fdb040f861463d712d7f3f99
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2023-05-30 11:07:53 +0700

    Update send deliverycode

--- XHTD_SERVICES_TRAM951_1/Jobs/Tram951ModuleJob.cs
+++ XHTD_SERVICES_TRAM951_1/Jobs/Tram951ModuleJob.cs
@@ -47,6 +47,8 @@ namespace XHTD_SERVICES_TRAM951_1.Jobs
 
         protected readonly string SCALE_SIGNALR_RFID_CODE = "TRAM951_1_RFID";
 
+        protected readonly string SCALE_DELIVERY_CODE = "TRAM951_1_DELIVERY_CODE";
+
         protected readonly string VEHICLE_STATUS = "VEHICLE_1_STATUS";
 
         protected const string SERVICE_ACTIVE_CODE = "TRAM951_1_ACTIVE";
@@ -343,6 +345,7 @@ namespace XHTD_SERVICES_TRAM951_1.Jobs
 
                                         new ScaleHub().SendMessage("Notification", $"{vehicleCodeCurrent} - RFID {cardNoCurrent} không có đơn hàng hợp lệ");
                                         new ScaleHub().SendMessage($"{VEHICLE_STATUS}", $"{vehicleCodeCurrent} - RFID {cardNoCurrent} không có đơn hàng hợp lệ");
+                                        new ScaleHub().SendMessage($"{SCALE_DELIVERY_CODE}", $"{vehicleCodeCurrent}");
 
                                         var newCardNoLog = new CardNoLog { CardNo = cardNoCurrent, DateTime = DateTime.Now };
                                         tmpInvalidCardNoLst.Add(newCardNoLog);
@@ -353,6 +356,7 @@ namespace XHTD_SERVICES_TRAM951_1.Jobs
                                     {
                                         new ScaleHub().SendMessage("Notification", $"{vehicleCodeCurrent} - RFID {cardNoCurrent} có đơn hàng hợp lệ");
                                         new ScaleHub().SendMessage($"{VEHICLE_STATUS}", $"{vehicleCodeCurrent} - RFID {cardNoCurrent} có đơn hàng hợp lệ");
+                                        new ScaleHub().SendMessage($"{SCALE_DELIVERY_CODE}", $"{vehicleCodeCurrent}");
 
                                         var newCardNoLog = new CardNoLog { CardNo = cardNoCurrent, DateTime = DateTime.Now };
                                         tmpCardNoLst.Add(newCardNoLog);
