f3f4bb42b4fba83f646dfa2e7702e573be825798
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2023-08-22 15:52:55 +0700

    Update config: sai so 1000

--- XHTD_SERVICES.Data/Common/ScaleConfig.cs
+++ XHTD_SERVICES.Data/Common/ScaleConfig.cs
@@ -16,7 +16,7 @@ namespace XHTD_SERVICES.Data.Common
 
         public static readonly int WEIGHT_SAISO = 20;
 
-        public static readonly int UNLADEN_WEIGHT_SAISO = 500;
+        public static readonly int UNLADEN_WEIGHT_SAISO = 1000;
 
         public static readonly int LADEN_WEIGHT_SAISO = 2000;
     }
