ddac53b6a2fc133281d71b25c23e7654a0f9facb
QUYET-PC\quyet <quyettm134@gmail.com>
2024-07-15 21:11:02 +0700

    thêm repos điểm xác thực

---
+++ XHTD_SERVICES.Data/Repositories/StoreOrderOperatingRepository.ConfirmationPoint.cs
@@ -0,0 +1,15 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XHTD_SERVICES.Data.Entities;
+
+namespace XHTD_SERVICES.Data.Repositories
+{
+    public partial class StoreOrderOperatingRepository : BaseRepository<tblStoreOrderOperating>
+    {
+
+    }
+}
