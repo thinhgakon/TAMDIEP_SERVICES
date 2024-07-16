791b6f6f01d1ac8bb0d82c83c6487d49e33a1516
quyettm134 <quyettm134@gmail.com>
2024-07-16 08:26:10 +0700

    thêm service XHTD_SERVICES_CONFIRM

---
+++ XHTD_SERVICES_CONFIRM/Service.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using log4net;
+using System.ServiceProcess;
+using XHTD_SERVICES_CONFIRM.Hubs;
+using XHTD_SERVICES_CONFIRM.Schedules;
+
+namespace XHTD_SERVICES_CONFIRM
+{
+    public partial class Service : ServiceBase
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log.Info("OnStart service GATEWAY");
+            Autofac.IContainer container = DIBootstrapper.Init();
+            container.Resolve<JobScheduler>().Start();
+
+            new SignalRService().OnStart(null);
+        }
+
+        protected override void OnStop()
+        {
+            log.Info("OnStop service GATEWAY");
+        }
+    }
+}
