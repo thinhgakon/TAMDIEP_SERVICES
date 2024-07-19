51607c1f89227b1e3cccee07aeb5eafdb7d708a1
quyettm134 <quyettm134@gmail.com>
2024-07-19 15:09:49 +0700

    fix CALL_IN_GATEWAY

---
+++ XHTD_SERVICES_CALL_IN_GATEWAY/Jobs/GatewayCallJob.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz;
+using log4net;
+using XHTD_SERVICES.Data.Repositories;
+using RestSharp;
+using XHTD_SERVICES.Data.Models.Response;
+using Newtonsoft.Json;
+using XHTD_SERVICES.Helper;
+using XHTD_SERVICES.Helper.Models.Request;
+using System.Threading;
+using XHTD_SERVICES.Data.Entities;
+
+namespace XHTD_SERVICES_CALL_IN_GATEWAY.Jobs
+{
+    public class GatewayCallJob : IJob
+    {
+        protected readonly StoreOrderOperatingRepository _storeOrderOperatingRepository;
+
+        protected readonly GatewayCallLogger _gatewayCallLogger;
+
+        public GatewayCallJob(
+            StoreOrderOperatingRepository storeOrderOperatingRepository,
+            GatewayCallLogger gatewayCallLogger
+            )
+        {
+            _storeOrderOperatingRepository = storeOrderOperatingRepository;
+            _gatewayCallLogger = gatewayCallLogger;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            await Task.Run(async () =>
+            {
+                SyncOrderProcess();
+            });
+        }
+
+        private void SyncOrderProcess()
+        {
+            _gatewayCallLogger.LogInfo("Service is running...");
+        }
+    }
+}
