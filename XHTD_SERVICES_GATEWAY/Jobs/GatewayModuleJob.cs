eaffca4852a62c778ad7860f39c0db9592e6ada7
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2024-07-26 21:11:00 +0700

    Gateway: #region Read RFID by C3-400

--- XHTD_SERVICES_GATEWAY/Jobs/GatewayModuleJob.cs
+++ XHTD_SERVICES_GATEWAY/Jobs/GatewayModuleJob.cs
@@ -205,18 +205,6 @@ namespace XHTD_SERVICES_GATEWAY.Jobs
             trafficLightOut = devices.FirstOrDefault(x => x.Code == "CBV.DGT-OUT");
         }
 
-        public void AuthenticateGatewayModule()
-        {
-            // 1. Connect Device
-            while (!DeviceConnected)
-            {
-                ConnectGatewayModule();
-            }
-
-            // 2. Đọc dữ liệu từ thiết bị
-            ReadDataFromC3400();
-        }
-
         public void AuthenticateGatewayModuleFromController()
         {
             // 1. Connect Device
@@ -244,39 +232,6 @@ namespace XHTD_SERVICES_GATEWAY.Jobs
             ReadDataFromPegasus();
         }
 
-        public bool ConnectGatewayModule()
-        {
-            var ipAddress = c3400?.IpAddress;
-            try
-            {
-                string str = $"protocol=TCP,ipaddress={ipAddress},port={c3400?.PortNumber},timeout=2000,passwd=";
-                int ret = 0;
-                if (IntPtr.Zero == h21)
-                {
-                    h21 = Connect(str);
-                    if (h21 != IntPtr.Zero)
-                    {
-                        _gatewayLogger.LogInfo($"Connected to C3-400 {ipAddress}");
-
-                        DeviceConnected = true;
-                    }
-                    else
-                    {
-                        _gatewayLogger.LogInfo($"Connect to C3-400 {ipAddress} failed");
-
-                        ret = PullLastError();
-                        DeviceConnected = false;
-                    }
-                }
-                return DeviceConnected;
-            }
-            catch (Exception ex)
-            {
-                _gatewayLogger.LogInfo($@"Connect to C3-400 {ipAddress} error: {ex.Message}");
-                return false;
-            }
-        }
-
         public bool ConnectGatewayModuleFromController()
         {
             _gatewayLogger.LogInfo("Thuc hien ket noi.");
@@ -301,73 +256,6 @@ namespace XHTD_SERVICES_GATEWAY.Jobs
                 _gatewayLogger.LogInfo(ex.Message);
                 _gatewayLogger.LogInfo(ex.StackTrace);
                 return false;
-            }
-        }
-
-        public async void ReadDataFromC3400()
-        {
-            _gatewayLogger.LogInfo("Reading RFID from C3-400 ...");
-
-            if (DeviceConnected)
-            {
-                while (DeviceConnected)
-                {
-                    int ret = 0, buffersize = 256;
-                    string str = "";
-                    string[] tmp = null;
-                    byte[] buffer = new byte[256];
-
-                    if (IntPtr.Zero != h21)
-                    {
-                        ret = GetRTLog(h21, ref buffer[0], buffersize);
-                        if (ret >= 0)
-                        {
-                            try
-                            {
-                                str = Encoding.Default.GetString(buffer);
-                                tmp = str.Split(',');
-
-                                // Bắt đầu xử lý khi nhận diện được RFID
-                                if (tmp[2] != "0" && tmp[2] != "")
-                                {
-                                    var cardNoCurrent = tmp[2]?.ToString();
-                                    var doorCurrent = tmp[3]?.ToString();
-                                    var timeCurrent = tmp[0]?.ToString();
-
-
-                                    // 1.Xác định xe cân vào / ra
-                                    var isLuongVao = doorCurrent == rfidVao1.PortNumberDeviceIn.ToString()
-                                                    || doorCurrent == rfidVao2.PortNumberDeviceIn.ToString();
-
-                                    var isLuongRa = doorCurrent == rfidRa1.PortNumberDeviceIn.ToString()
-                                                    || doorCurrent == rfidRa2.PortNumberDeviceIn.ToString();
-
-                                    await ReadDataProcess(cardNoCurrent, isLuongVao, isLuongRa);
-                                }
-                            }
-                            catch (Exception ex)
-                            {
-                                _gatewayLogger.LogError($@"Co loi xay ra khi xu ly RFID {ex.StackTrace} {ex.Message} ");
-                                continue;
-                            }
-                        }
-                        else
-                        {
-                            _gatewayLogger.LogWarn("No data. Reconnect ...");
-                            DeviceConnected = false;
-                            h21 = IntPtr.Zero;
-
-                            AuthenticateGatewayModule();
-                        }
-                    }
-                }
-            }
-            else
-            {
-                DeviceConnected = false;
-                h21 = IntPtr.Zero;
-
-                AuthenticateGatewayModule();
             }
         }
 
@@ -400,7 +288,6 @@ namespace XHTD_SERVICES_GATEWAY.Jobs
             }
 
         }
-
 
         public async void ReadDataFromController()
         {
@@ -461,7 +348,6 @@ namespace XHTD_SERVICES_GATEWAY.Jobs
                 AuthenticateGatewayModuleFromController();
             }
         }
-
 
         private async Task ReadDataProcess(string cardNoCurrent, bool isLuongVao, bool isLuongRa)
         {
@@ -1042,5 +928,119 @@ namespace XHTD_SERVICES_GATEWAY.Jobs
         {
             return BitConverter.ToString(b).Replace("-", "");
         }
+
+        #region Read RFID by C3-400
+        public void AuthenticateGatewayModule()
+        {
+            // 1. Connect Device
+            while (!DeviceConnected)
+            {
+                ConnectGatewayModule();
+            }
+
+            // 2. Đọc dữ liệu từ thiết bị
+            ReadDataFromC3400();
+        }
+
+        public bool ConnectGatewayModule()
+        {
+            var ipAddress = c3400?.IpAddress;
+            try
+            {
+                string str = $"protocol=TCP,ipaddress={ipAddress},port={c3400?.PortNumber},timeout=2000,passwd=";
+                int ret = 0;
+                if (IntPtr.Zero == h21)
+                {
+                    h21 = Connect(str);
+                    if (h21 != IntPtr.Zero)
+                    {
+                        _gatewayLogger.LogInfo($"Connected to C3-400 {ipAddress}");
+
+                        DeviceConnected = true;
+                    }
+                    else
+                    {
+                        _gatewayLogger.LogInfo($"Connect to C3-400 {ipAddress} failed");
+
+                        ret = PullLastError();
+                        DeviceConnected = false;
+                    }
+                }
+                return DeviceConnected;
+            }
+            catch (Exception ex)
+            {
+                _gatewayLogger.LogInfo($@"Connect to C3-400 {ipAddress} error: {ex.Message}");
+                return false;
+            }
+        }
+
+        public async void ReadDataFromC3400()
+        {
+            _gatewayLogger.LogInfo("Reading RFID from C3-400 ...");
+
+            if (DeviceConnected)
+            {
+                while (DeviceConnected)
+                {
+                    int ret = 0, buffersize = 256;
+                    string str = "";
+                    string[] tmp = null;
+                    byte[] buffer = new byte[256];
+
+                    if (IntPtr.Zero != h21)
+                    {
+                        ret = GetRTLog(h21, ref buffer[0], buffersize);
+                        if (ret >= 0)
+                        {
+                            try
+                            {
+                                str = Encoding.Default.GetString(buffer);
+                                tmp = str.Split(',');
+
+                                // Bắt đầu xử lý khi nhận diện được RFID
+                                if (tmp[2] != "0" && tmp[2] != "")
+                                {
+                                    var cardNoCurrent = tmp[2]?.ToString();
+                                    var doorCurrent = tmp[3]?.ToString();
+                                    var timeCurrent = tmp[0]?.ToString();
+
+
+                                    // 1.Xác định xe cân vào / ra
+                                    var isLuongVao = doorCurrent == rfidVao1.PortNumberDeviceIn.ToString()
+                                                    || doorCurrent == rfidVao2.PortNumberDeviceIn.ToString();
+
+                                    var isLuongRa = doorCurrent == rfidRa1.PortNumberDeviceIn.ToString()
+                                                    || doorCurrent == rfidRa2.PortNumberDeviceIn.ToString();
+
+                                    await ReadDataProcess(cardNoCurrent, isLuongVao, isLuongRa);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                _gatewayLogger.LogError($@"Co loi xay ra khi xu ly RFID {ex.StackTrace} {ex.Message} ");
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            _gatewayLogger.LogWarn("No data. Reconnect ...");
+                            DeviceConnected = false;
+                            h21 = IntPtr.Zero;
+
+                            AuthenticateGatewayModule();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                DeviceConnected = false;
+                h21 = IntPtr.Zero;
+
+                AuthenticateGatewayModule();
+            }
+        }
+        #endregion
     }
 }
