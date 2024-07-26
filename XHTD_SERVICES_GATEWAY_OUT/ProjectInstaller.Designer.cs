aad7afc227fac354b344c774c2a18f30c59ff153
PhatPX <phatpx2611@gmail.com>
2024-07-26 14:26:31 +0700

    Update service name

--- XHTD_SERVICES_GATEWAY_OUT/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_GATEWAY_OUT/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "TDGatewayService";
+            this.serviceInstaller1.ServiceName = "TDGatewayServiceOUT";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
