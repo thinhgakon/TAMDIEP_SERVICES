ccd11c48c6edfdd82c4a9a212fcfd18e414050f7
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2024-07-27 22:35:56 +0700

    Tram951-2: reconnect

--- XHTD_SERVICES_TRAM951_1/Jobs/ConnectPegasusJob.cs
+++ XHTD_SERVICES_TRAM951_1/Jobs/ConnectPegasusJob.cs
@@ -9,16 +9,14 @@ namespace XHTD_SERVICES_TRAM951_1.Jobs
 {
     public class ConnectPegasusJob : IJob
     {
-        public static int RefPort1 = 6000;
-        public static byte RefComAdr1 = 0xFF;
-        public static int RefPort2 = 6000;
-        public static byte RefComAdr2 = 0xFF;
-
+        private byte ComAddr = 0xFF;
+        private int PortHandle = 6000;
+        private string PegasusAdr = "192.168.13.182";
         protected readonly Logger _logger;
 
-        public ConnectPegasusJob(Logger logger)
+        public ConnectPegasusJob(Logger gatewayLogger)
         {
-            _logger = logger;
+            _logger = gatewayLogger;
         }
 
         public async Task Execute(IJobExecutionContext context)
@@ -36,46 +34,24 @@ namespace XHTD_SERVICES_TRAM951_1.Jobs
 
         public void CheckConnection()
         {
-            try
+            Ping pingSender = new Ping();
+            PingReply reply = pingSender.Send(PegasusAdr);
+
+            if (reply.Status == IPStatus.Success)
             {
-                Ping pingSender = new Ping();
-                PingReply reply = pingSender.Send(Program.PegasusIP1);
-
-                if (reply.Status == IPStatus.Success)
-                {
-                    //Console.WriteLine("Connection ok");
-                }
-                else
-                {
-                    var openresult = PegasusReader.Connect(RefPort1, Program.PegasusIP1, ref Program.RefComAdr1, ref Program.RefPort1);
-                    while (openresult != 0)
-                    {
-                        PegasusReader2.Close(Program.RefPort1);
-                        PegasusReader.Connect(RefPort1, Program.PegasusIP1, ref Program.RefComAdr1, ref Program.RefPort1);
-                        Thread.Sleep(1000);
-                    }
-                    _logger.LogWarn($"Connect {Program.PegasusIP1} fail. Start reconnect");
-                }
-
-                if (reply.Status == IPStatus.Success)
-                {
-                    //Console.WriteLine("Connection ok");
-                }
-                else
-                {
-                    var openresult = PegasusReader2.Connect(RefPort2, Program.PegasusIP2, ref Program.RefComAdr2, ref Program.RefPort2);
-                    while (openresult != 0)
-                    {
-                        PegasusReader2.Close(Program.RefPort2);
-                        openresult = PegasusReader2.Connect(RefPort2, Program.PegasusIP2, ref Program.RefComAdr2, ref Program.RefPort2);
-                        Thread.Sleep(1000);
-                    }
-                    _logger.LogWarn($"Connect {Program.PegasusIP2} fail. Start reconnect");
-                }
+                Console.WriteLine("Connection ok");
+                return;
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine($"{ex.Message}");
+                int port = PortHandle;
+                var openresult = PegasusStaticClassReader.OpenNetPort(PortHandle, PegasusAdr, ref ComAddr, ref port);
+                while (openresult != 0)
+                {
+                    openresult = PegasusStaticClassReader.OpenNetPort(PortHandle, PegasusAdr, ref ComAddr, ref port);
+                    Thread.Sleep(1000);
+                }
+                _logger.LogWarn("Connect fail. Start reconnect");
             }
         }
     }
