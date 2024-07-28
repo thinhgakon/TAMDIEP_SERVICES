421a3ec06fce341b09b75fecad108e5b3ef8521a
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2024-07-28 12:44:18 +0700

    Confirm: đổi tên service

--- XHTD_SERVICES_CONFIRM/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_CONFIRM/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "TDConfirmService";
+            this.serviceInstaller1.ServiceName = "XHTD_Service_Confirm";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
