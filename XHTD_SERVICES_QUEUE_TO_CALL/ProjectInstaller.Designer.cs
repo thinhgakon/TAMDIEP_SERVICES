5d186517d9cd77c1b03349bd0fdcc3ffab44c202
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2024-08-01 09:46:55 +0700

    QueueToCall: đổi tên service

--- XHTD_SERVICES_QUEUE_TO_CALL/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_QUEUE_TO_CALL/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "XHTDQueueToCallService";
+            this.serviceInstaller1.ServiceName = "XHTD_Service_QueueToCall";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
