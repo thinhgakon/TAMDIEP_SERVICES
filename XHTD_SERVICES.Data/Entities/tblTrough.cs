d50482e27e34ae9e800b01f74e86c3575245b9b1
quyettm134 <quyettm134@gmail.com>
2024-08-20 08:39:03 +0700

    update model database

--- XHTD_SERVICES.Data/Entities/tblTrough.cs
+++ XHTD_SERVICES.Data/Entities/tblTrough.cs
@@ -20,7 +20,7 @@ namespace XHTD_SERVICES.Data.Entities
             this.tblTroughTypeProducts = new HashSet<tblTroughTypeProduct>();
         }
     
-        public int Id { get; set; }
+        public Nullable<int> Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public string Machine { get; set; }
@@ -43,6 +43,7 @@ namespace XHTD_SERVICES.Data.Entities
         public Nullable<System.DateTime> UpdateDay { get; set; }
         public string UpdateBy { get; set; }
         public string ProductCategory { get; set; }
+        public Nullable<double> FirstSensorQuantityCurrent { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<tblTroughTypeProduct> tblTroughTypeProducts { get; set; }
