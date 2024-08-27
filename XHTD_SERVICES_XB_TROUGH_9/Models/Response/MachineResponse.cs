95271631f64612ab117040997da4db70aa6bd9af
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2024-08-27 15:24:33 +0700

    SLING: add serice máng 9

---
+++ XHTD_SERVICES_XB_TROUGH_9/Models/Response/MachineResponse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHTD_SERVICES_XB_TROUGH_9.Models.Response
+{
+    public class MachineResponse
+    {
+        public bool Status { get; set; }
+
+        public string Message { get; set; }
+
+        public object Data { get; set; }
+
+        public MessageObject MessageObject { get; set; }
+
+        public MachineResponse()
+        {
+            Status = true;
+            MessageObject = new MessageObject();
+        }
+    }
+
+    public class MessageObject
+    {
+        public string Code { get; set; }
+        public string Language { get; set; }
+        public string Message { get; set; }
+        public string MessageDetail { get; set; }
+        public string MessageType { get; set; } // S (Success), W (Warning), E (Error)
+        public string LogId { get; set; }
+        public MessageObject()
+        {
+            Code = string.Empty;
+            Message = string.Empty;
+            MessageDetail = string.Empty;
+            MessageType = string.Empty;
+            LogId = string.Empty;
+        }
+    }
+}
