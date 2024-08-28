1a25fa090828d4595062743616f1d121ff381287
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2024-08-28 21:31:45 +0700

    XB Led: add new led

--- XHTD_DEBUG/Program.cs
+++ XHTD_DEBUG/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Autofac;
 using log4net;
-using XHTD_SERVICES_SYNC_TROUGH;
-using XHTD_SERVICES_SYNC_TROUGH.Schedules;
+using XHTD_SERVICES_LED;
+using XHTD_SERVICES_LED.Schedules;
 
 namespace XHTD_DEBUG
 {
