c5a1b4af871bf78131d2be2f0f0eb10cdd27a7a8
quyettm134 <quyettm134@gmail.com>
2024-08-29 18:37:00 +0700

    Revert "thêm signalR trong Sync Trough"
    This reverts commit be59bcba55d85a17f38943bb08c53e100ad96b7c.

--- XHTD_SERVICES_SYNC_TROUGH/Jobs/SyncTroughJob12.cs
+++ XHTD_SERVICES_SYNC_TROUGH/Jobs/SyncTroughJob12.cs
@@ -20,7 +20,6 @@ using System.IO;
 using XHTD_SERVICES.Data.Models.Values;
 using System.Text.RegularExpressions;
 using System.Runtime.InteropServices.ComTypes;
-using XHTD_SERVICES_SYNC_TROUGH.Hubs;
 
 namespace XHTD_SERVICES_SYNC_TROUGH.Jobs
 {
@@ -298,11 +297,6 @@ namespace XHTD_SERVICES_SYNC_TROUGH.Jobs
             }
 
             return (string.Empty, string.Empty, string.Empty, string.Empty);
-        }
-
-        private void SendDataTrough(string deliveryCode, string troughCode, string machineCode, int firstQuantity, int lastQuantity)
-        {
-            new TroughHub().SendDataTrough(deliveryCode, machineCode, troughCode, firstQuantity, lastQuantity);
         }
 
         public void Dispose()
