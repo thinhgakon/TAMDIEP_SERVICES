dea2fe4799f43fa65c35ee18b62e7d7180a4a2dd
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2024-09-10 14:34:51 +0700

    TRAMCAN: add service CANVAO_2

---
+++ XHTD_SERVICES_CANVAO_2/Jobs/ConnectPegasusJob.cs
@@ -0,0 +1,75 @@
+﻿using Quartz;
+using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+using XHTD_SERVICES_CANVAO_2.Devices;
+using log4net;
+
+namespace XHTD_SERVICES_CANVAO_2.Jobs
+{
+    public class ConnectPegasusJob : IJob
+    {
+        ILog _logger = LogManager.GetLogger("ConnectFileAppender");
+
+        private byte ComAddr = 0xFF;
+        private int PortHandle = 6000;
+        private string PegasusAdr = "192.168.13.181";
+
+        public ConnectPegasusJob()
+        {
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            await Task.Run(() =>
+            {
+                CheckConnection();
+            });
+        }
+
+        public void CheckConnection()
+        {
+            try
+            {
+                Ping pingSender = new Ping();
+                PingReply reply = pingSender.Send(PegasusAdr);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    //Console.WriteLine("Connection ok");
+                    return;
+                }
+                else
+                {
+                    WriteLogInfo("Start reconnect...");
+
+                    int port = PortHandle;
+                    var openresult = PegasusStaticClassReader.OpenNetPort(PortHandle, PegasusAdr, ref ComAddr, ref port);
+                    while (openresult != 0)
+                    {
+                        openresult = PegasusStaticClassReader.OpenNetPort(PortHandle, PegasusAdr, ref ComAddr, ref port);
+                        Thread.Sleep(1000);
+                    }
+
+                    WriteLogInfo("Reconnect success");
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLogInfo($"PING ERROR: {ex.Message}");
+            }
+        }
+
+        public void WriteLogInfo(string message)
+        {
+            Console.WriteLine(message);
+            _logger.Info(message);
+        }
+    }
+}
