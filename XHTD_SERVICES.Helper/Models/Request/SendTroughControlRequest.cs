b6e78513aaeac8769b2894d034472418f4943bca
quyettm134 <quyettm134@gmail.com>
2024-09-11 08:10:51 +0700

    gửi signalR khi bắt đầu / kết thúc đếm bao

---
+++ XHTD_SERVICES.Helper/Models/Request/SendTroughControlRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHTD_SERVICES.Helper.Models.Request
+{
+    public class SendTroughControlRequest
+    {
+        public string MachineCode { get; set; }
+
+        public string TroughCode { get; set; }
+
+        public string DeliveryCode { get; set; }
+
+        public string Vehicle { get; set; }
+    }
+}
