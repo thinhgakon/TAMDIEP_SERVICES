200fe17a956dc547824efccac2634db4f68a0cb8
quyettm134 <quyettm134@gmail.com>
2024-09-12 18:00:04 +0700

    thêm led job CANVAO_2

---
+++ XHTD_SERVICES_CANVAO_2/Jobs/LedJob.cs
@@ -0,0 +1,87 @@
+﻿using Autofac;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XHTD_SERVICES_CANVAO_2.Devices;
+
+namespace XHTD_SERVICES_CANVAO_2.Jobs
+{
+    public class LedJob : IJob
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected readonly string SCALE_1_LED_IP = "192.168.13.180";
+
+        public LedJob()
+        {
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            await Task.Run(() =>
+            {
+                LEDProcess();
+            });
+        }
+
+        public void LEDProcess()
+        {
+            try
+            {
+                if (Program.scaleValuesForResetLight == null || Program.scaleValuesForResetLight.Count == 0)
+                {
+                    if (Program.IsFirstTimeResetLed)
+                    {
+                        Program.IsFirstTimeResetLed = false;
+
+                        string emptyDataCode = $"*[H1][C1]HE THONG CAN TU DONG[H2][C1]---[H3][C1]---[H4][Cy]---[!]";
+                        DisplayScreenLed(emptyDataCode);
+
+                        log.Info("Reset led");
+                    }
+                    else
+                    {
+                        log.Info("Khong co xe dang can => Ket thuc");
+                    }
+
+                    return;
+                }
+                else
+                {
+                    Program.IsFirstTimeResetLed = true;
+                }
+
+                var weightText = Program.scaleValuesForResetLight.LastOrDefault();
+                var vehicleText = Program.InProgressVehicleCode != null ? Program.InProgressVehicleCode.ToUpper() : "HE THONG CAN TU DONG";
+
+                string dataCode = $"*[H1][C1]{vehicleText}[H2][C1]{weightText}[H3][C1]---[H4][Cy]---[!]";
+                DisplayScreenLed(dataCode);
+            }
+            catch (Exception ex)
+            {
+                log.Info($"ERROR: {ex.Message}");
+            }
+        }
+
+        public void DisplayScreenLed(string dataCode)
+        {
+            log.Info($"Send led: dataCode= {dataCode}");
+
+            if (DIBootstrapper.Init().Resolve<TCPLedControl>().DisplayScreen(SCALE_1_LED_IP, dataCode))
+            {
+                log.Info($"LED CAN VAO - OK");
+            }
+            else
+            {
+                log.Info($"LED CAN VAO - FAILED: dataCode={dataCode}");
+            }
+        }
+    }
+}
