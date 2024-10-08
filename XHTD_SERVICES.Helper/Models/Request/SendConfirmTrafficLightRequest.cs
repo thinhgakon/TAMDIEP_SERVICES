3e1376dd33e1e7dc988e8eaf5da5392e455d4b41
quyettm134 <quyettm134@gmail.com>
2024-10-08 11:32:33 +0700

    gửi signalR đèn điểm xác thực

---
+++ XHTD_SERVICES.Helper/Models/Request/SendConfirmTrafficLightRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHTD_SERVICES.Helper.Models.Request
+{
+    public class SendConfirmTrafficLightRequest
+    {
+        public string Red { get; set; }
+
+        public string Green { get; set; }
+    }
+}
