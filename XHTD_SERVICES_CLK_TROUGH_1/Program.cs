8aff5632aad3185c32c7ba45579be897213102b2
THINH-MBF6-PC\DELL <toi.la.dan.xu.nghe@gmail.com>
2024-10-16 10:19:22 +0700

    Refactor connect anten

--- XHTD_SERVICES_CLK_TROUGH_1/Program.cs
+++ XHTD_SERVICES_CLK_TROUGH_1/Program.cs
@@ -26,6 +26,14 @@ namespace XHTD_SERVICES_CLK_TROUGH_1
 
         public static bool UHFConnected = false;
 
+        public static DateTime? SendFailPingLastTime = null;
+
+        public static int CountToSendFailPing = 0;
+
+        public static DateTime? SendFailOpenPortLastTime = null;
+
+        public static int CountToSendFailOpenPort = 0;
+
         static void Main()
         {
             ServiceBase[] ServicesToRun;
