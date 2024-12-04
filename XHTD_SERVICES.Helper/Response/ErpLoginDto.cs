3e2ce5de5577da33941472f3c44f81ef822c7470
quyetns <quyettm134@gmail.com>
2024-12-04 16:53:42 +0700

    thêm api token erp, update số lô đơn hàng

---
+++ XHTD_SERVICES.Helper/Response/ErpLoginDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHTD_SERVICES.Helper.Response
+{
+    public class ERPLoginRequestDto
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool? RememberMe { get; set; }
+    }
+
+    public class ERPLoginResultDto
+    {
+        public string Token { get; set; }
+
+        public string UserName { get; set; }
+
+        public string User_Id { get; set; }
+    }
+}
