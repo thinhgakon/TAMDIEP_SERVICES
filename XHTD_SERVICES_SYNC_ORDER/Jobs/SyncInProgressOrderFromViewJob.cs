8e03b822e52a55ff80aa631a6d66e536125c354e
quyetns <quyettm134@gmail.com>
2024-12-05 11:37:42 +0700

    thêm trường DELIVERY_CODE_TGC

--- XHTD_SERVICES_SYNC_ORDER/Jobs/SyncInProgressOrderFromViewJob.cs
+++ XHTD_SERVICES_SYNC_ORDER/Jobs/SyncInProgressOrderFromViewJob.cs
@@ -183,7 +183,7 @@ namespace XHTD_SERVICES_SYNC_ORDER.Jobs
 
             string query = $@"SELECT ORDER_ID, DELIVERY_CODE, TIMEIN, TIMEOUT, LOADWEIGHTNULL, STATUS, LOADWEIGHTFULL, PRODUCT_NAME, 
                                      VEHICLE_CODE, DRIVER_NAME, CUSTOMER_NAME, ORDER_QUANTITY, ORDER_DATE, MOOC_CODE, LOCATION_CODE, 
-                                     TRANSPORT_METHOD_ID, LAST_UPDATE_DATE, ITEM_CATEGORY, DOC_NUM, ORDER_REQ_ID, BLANKET_ID, TOP_SEAL_COUNT, TOP_SEAL_DES
+                                     TRANSPORT_METHOD_ID, LAST_UPDATE_DATE, ITEM_CATEGORY, DOC_NUM, ORDER_REQ_ID, BLANKET_ID, TOP_SEAL_COUNT, TOP_SEAL_DES, DELIVERY_CODE_TGC
                             FROM apps.dev_sales_orders_mbf_v 
                             WHERE LAST_UPDATE_DATE >= SYSTIMESTAMP - INTERVAL '{numberHoursSearchOrder}' HOUR";
 
@@ -211,7 +211,8 @@ namespace XHTD_SERVICES_SYNC_ORDER.Jobs
                 sourceDocumentId = reader["ORDER_REQ_ID"] != DBNull.Value ? reader["ORDER_REQ_ID"].ToString() :
                                    reader["BLANKET_ID"] != DBNull.Value ? reader["BLANKET_ID"].ToString() : null,
                 topSealCount = reader["TOP_SEAL_COUNT"]?.ToString(),
-                topSealDes = reader["TOP_SEAL_DES"]?.ToString()
+                topSealDes = reader["TOP_SEAL_DES"]?.ToString(),
+                deliveryCodeTgc = reader["DELIVERY_CODE_TGC"]?.ToString()
             };
 
             List<OrderItemResponse> result = oracleHelper.GetDataFromOracle(query, mapFunc);
