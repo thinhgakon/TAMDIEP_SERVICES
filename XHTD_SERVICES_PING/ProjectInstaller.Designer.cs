9983d4405fef27216477f38b0a432a4518b58d70
quyetns <quyettm134@gmail.com>
2024-12-20 15:22:23 +0700

    prod deploy

--- XHTD_SERVICES_PING/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_PING/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "HPPingService";
+            this.serviceInstaller1.ServiceName = "PROD_Service_Ping";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
