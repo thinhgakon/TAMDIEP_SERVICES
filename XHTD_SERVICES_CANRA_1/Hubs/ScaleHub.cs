36dd2b2dff326d7d89536ae2c53ded63f08aa8e6
DESKTOP-1H9TILT\THINH-BU <toi.la.dan.xu.nghe@gmail.com>
2025-04-11 14:09:48 +0700

    Remove unused code

--- XHTD_SERVICES_CANRA_1/Hubs/ScaleHub.cs
+++ XHTD_SERVICES_CANRA_1/Hubs/ScaleHub.cs
@@ -111,8 +111,6 @@ namespace XHTD_SERVICES_CANRA_1.Hubs
                     Program.EnabledRfidTime = null;
 
                     Program.IsLockingScale = false;
-
-                    await ReleaseScale();
                 }
 
                 if (Program.IsEnabledRfid && Program.EnabledRfidTime != null && Program.EnabledRfidTime < time.AddSeconds(-1 * TIME_TO_READ_RFID))
